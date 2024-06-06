125f1f0383788d98fde066faf53448ba65c264d9
hassan-mehedi <howlader.mehedihassan@gmail.com>
2024-06-06 17:07:12 +0600

    refactor: remove unnecessary states and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Box, Grid } from "@mui/material";
 
 import LeftSideComponent from "./components/Main/LeftSide.component";
@@ -8,53 +8,49 @@ import RightSideComponent from "./components/Main/RightSide.component";
 import "./App.css";
 
 const App = () => {
-    const [recordedUrl, setRecordedUrl] = useState("");
-    const mediaStream = useRef(null);
-    const mediaRecorder = useRef(null);
-    const chunks = useRef([]);
-    const [isRecording, setIsRecording] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
 
-    // New
-    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
-    const [inputPrompt, setInputPrompt] = useState("");
-    const [outputPrompt, setOutputPrompt] = useState("");
+  // New
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const [inputPrompt, setInputPrompt] = useState("");
+  const [outputPrompt, setOutputPrompt] = useState("");
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowHeight(window.innerHeight);
-        };
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight);
+    };
 
-        window.addEventListener("resize", handleResize);
+    window.addEventListener("resize", handleResize);
 
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-    return (
-        <Box width="100%">
-            <Grid container height="100vh" width="100%" padding={2}>
-                <Grid item height="100%" xs={3.5}>
-                    <LeftSideComponent
-                        inputPrompt={inputPrompt}
-                        setInputPrompt={setInputPrompt}
-                        outputPrompt={outputPrompt}
-                        setOutputPrompt={setOutputPrompt}
-                        windowHeight={windowHeight}
-                        isRecording={isRecording}
-                        setIsRecording={setIsRecording}
-                    />
-                </Grid>
+  return (
+    <Box width="100%">
+      <Grid container height="100vh" width="100%" padding={2}>
+        <Grid item height="100%" xs={3.5}>
+          <LeftSideComponent
+            inputPrompt={inputPrompt}
+            setInputPrompt={setInputPrompt}
+            outputPrompt={outputPrompt}
+            setOutputPrompt={setOutputPrompt}
+            windowHeight={windowHeight}
+            isRecording={isRecording}
+            setIsRecording={setIsRecording}
+          />
+        </Grid>
 
-                <Grid item height="100%" xs={5}>
-                    <MiddleSideComponent />
-                </Grid>
+        <Grid item height="100%" xs={5}>
+          <MiddleSideComponent />
+        </Grid>
 
-                <Grid item height="100%" xs={3.5}>
-                    <RightSideComponent />
-                </Grid>
-            </Grid>
-        </Box>
-    );
+        <Grid item height="100%" xs={3.5}>
+          <RightSideComponent />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 export default App;
